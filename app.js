409adbaea923676442c3ebccb592b8e17ca9f79d
Nadeem Hussain <hoteleyenadeem@gmail.com>
2021-05-12 02:59:30 -0700

    Practise set

---
+++ app.js
@@ -0,0 +1,60 @@
+// Chapter No 1. Alert.                                               1
+alert("Thanks you for your input !");
+var a = "Nadeem ";
+var b = "Love with me";
+var result = a + b;
+alert(result);
+var name = "Nadeem Hussain";
+console.log(name);
+
+// Chapter No 2 .Variable for Strings.                                2
+var test;
+test = "pass";
+console.log(test);
+var nationality;
+nationality = "Pakistan , India , America";
+document.write(nationality);
+var name = "Moiz baba";
+var age = 4;
+var lang = "Urdu";
+var nationality = "Pakistani";
+var study = "Holy Quran";
+console.log(name);
+console.log(age);
+console.log(nationality);
+console.log(study);
+
+// Chapter No 3 .Variables for Numbers.                                3
+var thanx = "thanks for your input !";
+alert(thanx);
+var num1 = 100;
+var num2 = 25;
+var result = num1 + num2;
+console.log(result);
+var originalNum = 12;
+var newNum = originalNum + 8;
+console.log(newNum);
+var originalNum = 62;
+var addNumTo = originalNum + 12;
+console.log(addNumTo);
+var marks = 406;
+marks = marks + 200;
+console.log(marks);
+var a = "Num";
+a = "Num " + 5;
+alert(a);
+
+// Chapter No 4 . Variables Name Legal and IIIegal.                    4
+var rose = "Flowers";
+console.log(rose);
+var num_1 = 43;
+alert(num_1);      //(Good Practise)
+var bool = true;
+console.log(bool);
+// var 1bed = 23; ........... (Bed Practise)
+// console.log(1bed);
+var oldNumber = 22;
+var newNumber = oldNumber + 8;
+alert(newNumber);       //(camelCase Practise) 
+
+// Chapter No 4 . 
