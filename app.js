3fcac28517f9811d8bc9607b4903b8abc86fb0e5
Nadeem Hussain <hoteleyenadeem@gmail.com>
2021-06-04 18:44:31 +0500

    javasripte

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ alert("Thanks you for your input !");
 var a = "Nadeem ";
 var b = "Love with me";
 var result = a + b;
-alert(result);
+console.log(result);
 var name = "Nadeem Hussain";
 console.log(name);
 
@@ -26,7 +26,7 @@ console.log(study);
 
 // Chapter No 3 .Variables for Numbers.                                3
 var thanx = "thanks for your input !";
-alert(thanx);
+console.log(thanx);
 var num1 = 100;
 var num2 = 25;
 var result = num1 + num2;
@@ -37,25 +37,28 @@ console.log(newNum);
 var originalNum = 62;
 var addNumTo = originalNum + 12;
 console.log(addNumTo);
+var oldNum = 10;
+newNum = oldNum + 90;
+console.log(newNum); 
 var marks = 406;
 marks = marks + 200;
 console.log(marks);
 var a = "Num";
 a = "Num " + 5;
-alert(a);
+console.log(a);
 
 // Chapter No 4 . Variables Name Legal and IIIegal.                    4
 var rose = "Flowers";
 console.log(rose);
 var num_1 = 43;
-alert(num_1);      //(Good Practise)
+console.log(num_1);      //(Good Practise)
 var bool = true;
 console.log(bool);
 // var 1bed = 23; ........... (Bed Practise)
 // console.log(1bed);
 var oldNumber = 22;
 var newNumber = oldNumber + 8;
-alert(newNumber);       //(camelCase Practise) 
+console.log(newNumber);       //(camelCase Practise) 
 
 // Chapter No 4 . 
 
